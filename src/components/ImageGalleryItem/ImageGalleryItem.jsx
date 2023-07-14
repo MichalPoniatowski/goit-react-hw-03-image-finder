05f353f953a46eaebb4b67e9e9e9f7e96ec9e889
MichalPoniatowski <m.poniatowski87@gmail.com>
2023-07-14 13:42:52 +0200

    axios added to dependiencies

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,10 +1,11 @@
 // import css from './ImageGalleryItem.module.css';
+import { nanoid } from 'nanoid';
 
 export const ImageGalleryItem = ({ data }) => {
   return (
     <>
-      {data.map(({ largeImageURL, webformatURL, tags, id }) => (
-        <li className="ImageGalleryItem" key={id}>
+      {data.map(({ largeImageURL, webformatURL, tags }) => (
+        <li className="ImageGalleryItem" key={nanoid()}>
           <div>
             <a href={largeImageURL}>
               <img
